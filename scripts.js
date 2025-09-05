9887601c66aea2292756a44eb5330e37554760be
joaocoqueiro <pbarros215@gmail.com>
2025-09-05 17:45:13 -0300

    Aprimorando CSS / JS Parcialmente finalizado

--- scripts.js
+++ scripts.js
@@ -1,41 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //Elementos DOM
+    // Elementos DOM
     const themeToggle = document.getElementById('theme-toggle');
-    const senhaDisplay = document.getElementById('senha');
+    const passwordDisplay = document.getElementById('password');
     const copyBtn = document.getElementById('copy-btn');
-    const gerarBtn = document.getElementById('generate-btn');
+    const genarateBtn = document.getElementById('generate-btn');
     const lengthRange = document.getElementById('length');    
     const lengthValue = document.getElementById('length-value');
     const upperCaseCheckbox = document.getElementById('uppercase');
     const numCheckbox = document.getElementById('numbers');
-    const simbolosCheckbox = document.getElementById('symbols');
+    const symbolsCheckbox = document.getElementById('symbols');
 
     // Caracteres para gerar senha
     const lowerCaseChars = 'abcdefghijklmnopqrstuvwyxz';
     const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWYXZ';
     const num = '0123456789';
-    const simbolosChars = '!@#$%&*()_+-=[]{}|;:,.<>?';
+    const symbolsChars = '!@#$%&*()_+-=[]{}|;:,.<>?';
 
-    //Atualizar valor do range
+    // Atualizar valor do range
     lengthRange.addEventListener('input', function() {
         lengthValue.textContent = this.value;
     })
 
-    //Gerar senha
-    gerarBtn.addEventListener('click', gerarSenha);
+    // Gerar senha
+    genarateBtn.addEventListener('click', gerarSenha);
 
     function gerarSenha() {
         let characteres = lowerCaseChars;
         let senha = '';
 
-        //Adiciona caracteres selecionados
+        // Adiciona caracteres selecionados
         if(upperCaseCheckbox.checked) characteres += upperCaseChars;
         if(numCheckbox.checked) characteres += num;
-        if(simbolosCheckbox.checked) characteres += simbolosChars;
+        if(symbolsCheckbox.checked) characteres += symbolsChars;
 
-        /*for(i = 0, i < lengthRange.value, i++) {
-            const randomIndex = 
-        };
-        */
+        for(let i = 0; i < lengthRange.value; i++) {
+            const randomIndex = Math.floor(Math.random() * characteres.length);
+            senha += characteres[randomIndex];
+        }
+
+        // Exibir senha
+        passwordDisplay.value = senha;
     }
-})
+
+    // Copiar senha
+    copyBtn.addEventListener('click', function(){
+        if(!passwordDisplay.value) return;
+        
+        navigator.clipboard.writeText(passwordDisplay.value)
+        .then(() => {
+            //Feedback Visual 
+            const originalText = copyBtn.textContent;
+            this.textContent = '✅';
+
+            setTimeout (() => {
+                copyBtn.textContent = originalText;
+            }, 2000);
+        });
+    });
+
+    // Alternar light mode
+    themeToggle.addEventListener('click', function(){
+        document.body.classList.toggle('light-mode');
+        this.textContent = document.body.classList.contains('light-mode') ? '☀️' : '🌙';
+    })
+
+    // Gerar senha inicial
+    gerarSenha();
+});
